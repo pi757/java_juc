6978602979bff0b12ad9b6ed2918b14ff6050299
jiaSong.pi <pi_757@163.com>
2022-07-06 00:08:46 +0800

    init

---
+++ src/main/java/cn/pjs/java_juc/JavaJucApplication.java
@@ -0,0 +1,13 @@
+package cn.pjs.java_juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJucApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/pjs/java_juc/JavaJucApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pjs.java_juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
